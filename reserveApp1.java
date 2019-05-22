4a2217f92695a4e527a90461a05f78dbdaaaa19d
parkjonghyun00 <43666040+parkjonghyun00@users.noreply.github.com>
2019-05-23 05:16:04 +0900

    test
    test

---
+++ reserveApp1.java
@@ -0,0 +1,36 @@
+/*	version 	: 1.0.2
+ * 	name		: 4조
+ * 	Last date	: 19.05.23
+ * contents		: 미용실 앱
+ * */
+
+// 미용사 변수 
+class Teacherinformation{
+	
+}
+// 회원 변수
+class Memberinformation{
+	
+}
+//
+abstract class HairContents{
+	
+}
+// 
+class HairReserve extends HairContents{
+	Teacherinformation ti = new Teacherinformation();
+	Memberinformation mi = new Memberinformation();
+	
+	
+	public void run(){
+		
+	}
+}
+// 예약App (실행)
+public class reserveApp {
+	public void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
